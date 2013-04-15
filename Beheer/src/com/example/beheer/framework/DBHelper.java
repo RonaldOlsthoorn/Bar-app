563cae459472919950dca71aa50c53f2701ee659
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2013-04-15 11:03:40 +0200

    Jow

--- Beheer/src/com/example/beheer/framework/DBHelper.java
+++ Beheer/src/com/example/beheer/framework/DBHelper.java
@@ -1,121 +1,54 @@
-package framework;
+package com.example.beheer.framework;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.SQLException;
+import com.example.beheer.MyApplication;
+
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
-import android.util.Log;
 
-public class DB {
+public class DBHelper extends SQLiteOpenHelper {
 
 	public static final String TAG = "DB";
 	public static int DATABASE_VERSION = 1;
 	public static final String DATABASE_NAME = "GrooverMembers.db";
-	private DBHelper dbHelper; // add final
-	private static DB singleton;
 
-	/*
-	 * Basic singleton getter and constructor. Singleton pattern used for thread
-	 * safety of the database
-	 */
-	public static DB getDB(Context context) {
+	private static DBHelper singleton;
+
+	public static DBHelper getDBHelper() {
 
 		if (singleton == null) {
-			singleton = new DB(context);
+			singleton = new DBHelper();
+
 		}
 		return singleton;
+
 	}
 
-	public DB(Context context) {
-
-		this.dbHelper = new DBHelper(context);
-		Log.i(TAG, "initialized data " + this.toString());
+	private DBHelper() {
+		super(MyApplication.getAppContext(), DATABASE_NAME, null,
+				DATABASE_VERSION);
+		// TODO Auto-generated constructor stub
 	}
 
-	/*
-	 * SELECT methods
-	 */
-	public Cursor getCategories() {
-
-		SQLiteDatabase db = this.dbHelper.getReadableDatabase();
-		return db.query(ItemList.TABLE_NAME, new String[]{ItemList.COLUMN_ID, ItemList.COLUMN_NAME_CAT}, null, null, ItemList.COLUMN_NAME_CAT, null,
-				ItemList.COLUMN_NAME_CAT + " COLLATE NOCASE");	
-	}
-	
-	public Cursor getArticles() {
-		
-		SQLiteDatabase db = this.dbHelper.getReadableDatabase();
-		return db.query(ItemList.TABLE_NAME, new String[]{ItemList.COLUMN_ID, ItemList.COLUMN_NAME_ITEM, ItemList.COLUMN_NAME_PRICE, ItemList.COLUMN_NAME_CAT}, null, null, null, null,
-				ItemList.COLUMN_NAME_CAT + " COLLATE NOCASE, "+ItemList.COLUMN_NAME_ITEM+" COLLATE NOCASE");	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(MemberTable.SQL_CREATE_TABLE);
+		db.execSQL(GroupTable.SQL_CREATE_TABLE);
+		db.execSQL(GroupMembers.SQL_CREATE_TABLE);
+		db.execSQL(ItemList.SQL_CREATE_TABLE);
 	}
 
-	/*
-	 * INSERT methods
-	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		db.execSQL(MemberTable.SQL_DELETE_ENTRIES);
+		db.execSQL(GroupTable.SQL_DELETE_ENTRIES);
+		db.execSQL(GroupMembers.SQL_DELETE_ENTRIES);
+		db.execSQL(ItemList.SQL_DELETE_ENTRIES);
 
-	@SuppressWarnings("finally")
-	public boolean insertOrIgnore(String table, ContentValues values){
-		
-		boolean res = false;
-		Log.d(TAG, "insertOrIgnore on " + values);
-		SQLiteDatabase db = this.dbHelper.getWritableDatabase();
-		try {
-			db.insertOrThrow(table, null, values);
-			res = true;
-
-		} catch (SQLException e) {
-			res = false;
-		} finally {
-			db.close();
-			return res;
-		}
-	}
-	/*
-	 * UPDATE methods
-	 */
-
-	/*
-	 * COMBINATION methods
-	 */
-
-	/*
-	 * closes the database connection
-	 */
-
-	public void close() {
-		this.dbHelper.close();
-	}
-
-	private class DBHelper extends SQLiteOpenHelper {
-
-		private DBHelper(Context context) {
-			super(context, DATABASE_NAME, null, DATABASE_VERSION);
-			// TODO Auto-generated constructor stub
-		}
-
-		@Override
-		public void onCreate(SQLiteDatabase db) {
-			// TODO Auto-generated method stub
-			db.execSQL(MemberTable.SQL_CREATE_TABLE);
-			db.execSQL(GroupTable.SQL_CREATE_TABLE);
-			db.execSQL(GroupMembers.SQL_CREATE_TABLE);
-			db.execSQL(ItemList.SQL_CREATE_TABLE);
-		}
-
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			// TODO Auto-generated method stub
-			db.execSQL(MemberTable.SQL_DELETE_ENTRIES);
-			db.execSQL(GroupTable.SQL_DELETE_ENTRIES);
-			db.execSQL(GroupMembers.SQL_DELETE_ENTRIES);
-			db.execSQL(ItemList.SQL_DELETE_ENTRIES);
-
-			onCreate(db);
-			DATABASE_VERSION = newVersion;
-		}
+		onCreate(db);
+		DATABASE_VERSION = newVersion;
 	}
 
 	public static abstract class MemberTable implements BaseColumns {
@@ -140,7 +73,8 @@ public class DB {
 				+ COLUMN_ACCOUNT
 				+ " INTEGER "
 				+ ","
-				+ COLUMN_BALANCE + " DOUBLE" + " )";
+				+ COLUMN_BALANCE
+				+ " DOUBLE" + " )";
 
 		public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
 				+ TABLE_NAME;
@@ -173,6 +107,7 @@ public class DB {
 		public static final String TABLE_NAME = "group_members";
 		public static final String COLUMN_NAME_GROUP_ID = "group_id";
 		public static final String COLUMN_NAME_MEMBER_ID = "member_id";
+
 		public static final String SQL_CREATE_TABLE = "CREATE TABLE  IF NOT EXISTS "
 				+ TABLE_NAME
 				+ " ("
@@ -208,8 +143,8 @@ public class DB {
 		public static final String COLUMN_ID = _ID;
 		public static final String COLUMN_NAME_ITEM = "item_name";
 		public static final String COLUMN_NAME_PRICE = "item_price";
-		public static final String COLUMN_NAME_DESC = "item_description";
 		public static final String COLUMN_NAME_CAT = "item_category";
+
 		public static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS "
 				+ TABLE_NAME
 				+ " ("
@@ -219,19 +154,16 @@ public class DB {
 				+ " TEXT NOT NULL , "
 				+ COLUMN_NAME_PRICE
 				+ " DOUBLE ,"
-				+ COLUMN_NAME_DESC
-				+ " TEXT, "
-				+ COLUMN_NAME_CAT
-				+ " TEXT DEFAULT 'overig' )";
+				+ COLUMN_NAME_CAT + " TEXT DEFAULT 'overig' )";
 		public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
 				+ TABLE_NAME;
 
 	}
-	
+
 	public static abstract class AccountList implements BaseColumns {
-		
+
 		public static final String TABLE_NAME = "account_list";
-		public static final String COLUMN_ACCOUNT = "account";
+		public static final String COLUMN_ACCOUNT = _ID;
 		public static final String COLUMN_TYPE = "type";
 		public static final String SQL_CREATE_TABLE = "CREATE TABLE  IF NOT EXISTS "
 				+ TABLE_NAME
@@ -240,11 +172,87 @@ public class DB {
 				+ " INTEGER PRIMARY KEY AUTOINTCREMENT"
 				+ ","
 				+ COLUMN_TYPE
-				+ " TEXT NOT NULL"
+				+ " TEXT NOT NULL" + ")";
+
+		public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
+				+ TABLE_NAME;
+	}
+
+	public static abstract class Order implements BaseColumns {
+
+		public static final String TABLE_NAME = "orders";
+		public static final String COLUMN_ID = _ID;
+		public static final String COLUMN_TOTAL = "total_amount";
+		public static final String COLUMN_ACCOUNT = "client_account";
+		public static final String COLUMN_TS_CREATED = "ts_created";
+		public static final String COLUMN_TS_SETTLED = "ts_settled";
+
+		public static final String SQL_CREATE_TABLE = "CREATE TABLE  IF NOT EXISTS "
+				+ TABLE_NAME
+				+ " ("
+				+ COLUMN_ID
+				+ " INTEGER PRIMARY KEY AUTOINTCREMENT"
+				+ ","
+				+ COLUMN_TOTAL
+				+ " DOUBLE"
+				+ ","
+				+ COLUMN_ACCOUNT
+				+ " INTEGER NOT NULL"
+				+ ","
+				+ COLUMN_TS_CREATED
+				+ " TIMESTAMP"
+				+ ","
+				+ COLUMN_TS_SETTLED
+				+ " TIMESTAMP" + ")";
+
+		public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
+				+ TABLE_NAME;
+	}
+
+	public static abstract class Consumption implements BaseColumns {
+
+		public static final String TABLE_NAME = "consumptions";
+		public static final String COLUMN_ID = _ID;
+		public static final String COLUMN_ARTICLE = "article";
+		public static final String COLUMN_AMMOUNT = "ammount";
+
+		public static final String SQL_CREATE_TABLE = "CREATE TABLE  IF NOT EXISTS "
+				+ TABLE_NAME
+				+ " ("
+				+ COLUMN_ID
+				+ " INTEGER PRIMARY KEY"
+				+ ","
+				+ COLUMN_ARTICLE
+				+ " INTEGER"
+				+ ","
+				+ COLUMN_AMMOUNT
+				+ " INTEGER" + ")";
+
+		public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
+				+ TABLE_NAME;
+	}
+
+	public static abstract class GroupClearances implements BaseColumns {
+
+		public static final String TABLE_NAME = "clearance";
+		public static final String COLUMN_ID = _ID;
+		public static final String COLUMN_GROUP = "group_id";
+		public static final String COLUMN_GROUP_NAME = "group_name";
+
+		public static final String SQL_CREATE_TABLE = "CREATE TABLE  IF NOT EXISTS "
+				+ TABLE_NAME
+				+ " ("
+				+ COLUMN_ID
+				+ " INTEGER PRIMARY KEY"
+				+ ","
+				+ COLUMN_GROUP
+				+ " INTEGER"
+				+ ","
+				+ COLUMN_GROUP_NAME
+				+ " TEXT"
 				+ ")";
 
 		public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
 				+ TABLE_NAME;
 	}
-	
 }
