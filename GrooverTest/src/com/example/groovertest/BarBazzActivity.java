8ec830b9a4a4679e6c88164920e37fc7df10b4fc
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2013-05-18 15:03:50 +0200

    jow

--- GrooverTest/src/com/example/groovertest/BarBazzActivity.java
+++ GrooverTest/src/com/example/groovertest/BarBazzActivity.java
@@ -1,30 +1,25 @@
 package com.example.groovertest;
 
-
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
+public class BarBazzActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_bar_bazz);
 		// Show the Up button in the action bar.
-		//getActionBar().setDisplayHomeAsUpEnabled(true);
-		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_bar_bazz, menu);
 		return true;
 	}
 
@@ -44,16 +39,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void toBeheer(View view){
-		
-		Intent intent = new Intent(this, BeheerActivity.class);
-		startActivity(intent);
-	}
-	
-	public void toBarBazz(View view){
-		
-		Intent intent = new Intent(this, BarBazzActivity.class);
-		startActivity(intent);
-	}
+
 }
