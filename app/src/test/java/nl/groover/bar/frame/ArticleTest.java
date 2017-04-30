a03a5d737ade87312b2b0e9dd01d82933d881f93
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2017-04-30 10:42:59 +0200

    gradle updates

---
+++ app/src/test/java/nl/groover/bar/frame/ArticleTest.java
@@ -0,0 +1,56 @@
+package nl.groover.bar.frame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ronald Olsthoorn on 3/3/2017.
+ */
+public class ArticleTest {
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void getEditable() throws Exception {
+
+    }
+
+    @Test
+    public void setName() throws Exception {
+
+    }
+
+    @Test
+    public void setPrice() throws Exception {
+
+    }
+
+    @Test
+    public void setColor() throws Exception {
+
+    }
+
+    @Test
+    public void setEditable() throws Exception {
+
+    }
+
+    @Test
+    public void getColor() throws Exception {
+
+    }
+
+}
---
+++ app/src/test/java/nl/groover/bar/frame/CustomerTest.java
@@ -0,0 +1,31 @@
+package nl.groover.bar.frame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ronald Olsthoorn on 3/3/2017.
+ */
+public class CustomerTest {
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void getAccount() throws Exception {
+
+    }
+
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+}
